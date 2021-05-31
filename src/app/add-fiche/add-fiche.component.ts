b3a14f03d4ad7dd2e082d108153bca4324b4616a
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-05-31 12:10:40 +0200

    Ajout des photos mockups et creation du component add-fiche

---
+++ src/app/add-fiche/add-fiche.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-fiche',
+  templateUrl: './add-fiche.component.html',
+  styleUrls: ['./add-fiche.component.scss']
+})
+export class AddFicheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { FicheComponent } from './fiche/fiche.component';
+import { AddFicheComponent } from './add-fiche/add-fiche.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FicheComponent
+    FicheComponent,
+    AddFicheComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatButtonModule,
     MatMenuModule,
     MatGridListModule,
     MatCardModule,
